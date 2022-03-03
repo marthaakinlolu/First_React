a1646956b0e5331e5477d333e3f2b13670a059ff
martha.akinlolu <marthaakinlolu@gmail.com>
2022-03-03 13:57:56 +0100

    update on Tesla clone

--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,14 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 function Section() {
   return (
     <Wrap>
-        Section
+        <ItemText>
+          <h1> Model S</h1>
+          <p>Order online for Touchless Delivery</p>
+        </ItemText>
+
+        <ButtonGroup>
+          <LeftButton>
+            Custom Order
+          </LeftButton>
+          <RightButton>
+            Existing Inventory
+          </RightButton>
+        </ButtonGroup>
+    
+    
     </Wrap>
   )
 }
 
 export default Section
 
-const Wrap = styled.div
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    backgroung-position: center;
+    background-repeat: no-repeat;
+    background-image: url('/images/images/model-s.jpg');
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+`
+const RightButton = styled(LeftButton)`
+
+`
